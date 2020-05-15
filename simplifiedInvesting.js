6fc06a94127697013b738094ee7cda4ad76f7988
HaiderKhan1 <47333610+HaiderKhan1@users.noreply.github.com>
2020-05-14 23:06:36 -0400

    Addition of the profitability ratios page

--- simplifiedInvesting.js
+++ simplifiedInvesting.js
@@ -1,7 +1,6 @@
 
 // MARKET PROSPECT RATIOS JS CODE //
 
-
 function calcEPS() {
 var netIncome;
 var pd;
@@ -28,7 +27,6 @@ if (isNaN(netIncome) || isNaN(pd) || isNaN(denom)) {
     }
 
 document.getElementById("EPS").innerHTML = "Earning Per Share Ratio: " + finalAnswer;
-
 }
 
 function pE(){
@@ -250,5 +248,104 @@ function debtRatio(){
       }
 
   document.getElementById("debt_Ratio").innerHTML = "Debt Ratio: " + finalAnswer;
+
+}
+
+//Profitability RATIOS //
+
+function grossMargin(){
+
+  var cogs, netSales, gross_Margin, finalAnswer;
+
+  cogs = Number(document.getElementById("cogs").value);
+  netSales = Number(document.getElementById("netSales").value);
+
+  if (isNaN(cogs) || isNaN(netSales)) {
+
+    finalAnswer = "Hmm looks like your input was not accepted <br> Please check your input: <br> (1) Is just numbers <br> (2) There are no alphabets <br> (3) No special characters other then decimal <br> (4) All input feilds have been filled";
+
+  } else if ((cogs == "") || (netSales == "")){
+    alert("All Input Feilds Must Be Filled");
+
+      } else {
+
+      gross_Margin = netSales - cogs;
+      finalAnswer = gross_Margin / netSales;
+
+      }
+
+  document.getElementById("grossMargin").innerHTML = "Gross Margin Ratio: " + finalAnswer;
+
+}
+
+function profitMargin(){
+
+  var netIncome, net_Sales, finalAnswer;
+
+  netIncome = Number(document.getElementById("netIncome").value);
+  net_Sales = Number(document.getElementById("net_Sales").value);
+
+  if (isNaN(netIncome) || isNaN(net_Sales)) {
+
+    finalAnswer = "Hmm looks like your input was not accepted <br> Please check your input: <br> (1) Is just numbers <br> (2) There are no alphabets <br> (3) No special characters other then decimal <br> (4) All input feilds have been filled";
+
+  } else if ((netIncome == "") || (net_Sales == "")){
+    alert("All Input Feilds Must Be Filled");
+
+      } else {
+
+      finalAnswer = netIncome / net_Sales;
+
+      }
+
+  document.getElementById("profitMargin").innerHTML = "Profit Margin Ratio: " + finalAnswer;
+
+}
+
+function returnOnAssets(){
+
+  var NetIncome, totalAssets, finalAnswer;
+
+  NetIncome = Number(document.getElementById("NetIncome").value);
+  totalAssets = Number(document.getElementById("totalAssets").value);
+
+  if (isNaN(NetIncome) || isNaN(totalAssets)) {
+
+    finalAnswer = "Hmm looks like your input was not accepted <br> Please check your input: <br> (1) Is just numbers <br> (2) There are no alphabets <br> (3) No special characters other then decimal <br> (4) All input feilds have been filled";
+
+  } else if ((NetIncome == "") || (totalAssets == "")){
+    alert("All Input Feilds Must Be Filled");
+
+      } else {
+
+      finalAnswer = NetIncome / totalAssets;
+
+      }
+
+  document.getElementById("returnOnAssets").innerHTML = "Return on Assets Ratio: " + finalAnswer;
+
+}
+
+function returnOnEquity(){
+
+  var Net_Income, sE, finalAnswer;
+
+  Net_Income = Number(document.getElementById("Net_Income").value);
+  sE = Number(document.getElementById("sE").value);
+
+  if (isNaN(Net_Income) || isNaN(sE)) {
+
+    finalAnswer = "Hmm looks like your input was not accepted <br> Please check your input: <br> (1) Is just numbers <br> (2) There are no alphabets <br> (3) No special characters other then decimal <br> (4) All input feilds have been filled";
+
+  } else if ((Net_Income == "") || (sE == "")){
+    alert("All Input Feilds Must Be Filled");
+
+      } else {
+
+      finalAnswer = Net_Income / sE;
+
+      }
+
+  document.getElementById("returnOnEquity").innerHTML = "Return on Equity Ratio: " + finalAnswer;
 
 }
